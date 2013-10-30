16cfc395d3602a1709d92b10017493ad533dc8b6
bryan0806 <chunwei.liu@gmail.com>
2013-10-30 19:51:24 +0800

    Xcode project built

---
+++ CPP week 2/CPP week 2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CPP week 2
+//
+//  Created by mac on 2013/10/30.
+//  Copyright (c) 2013年 mac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
